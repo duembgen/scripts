11463dedcb7c4ab17d5db8a579db0ab0beba3b82
duembgen <frederike.duembgen@epfl.ch>
2021-05-03 10:20:00 +0200

    Add pre-commit configuration

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+setup.py: 
+"""
+from setuptools import setup
+
+setup(
+    name='scripts',
+    version='0.0.1',
+    description='useful system-wide scripts',
+    url='http://github.com/duembgen/scripts',
+    author='Frederike Duembgen',
+    author_email='frederike.duembgen@gmail.com',
+    license='CCBY',
+    scripts=['remove_output'],
+    packages=[],
+    zip_safe=False
+)
