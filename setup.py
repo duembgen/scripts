22fe2f2d51b4ce0e7294c5c5c2a3c0ca0dbb84d3
duembgen <frederike.duembgen@epfl.ch>
2021-05-03 10:51:40 +0200

    Create version v0.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='scripts',
-    version='0.0.1',
+    version='v0.0.5',
     description='useful system-wide scripts',
     url='http://github.com/duembgen/scripts',
     author='Frederike Duembgen',
@@ -16,5 +16,6 @@ setup(
     license='CCBY',
     scripts=['remove_output'],
     packages=[],
-    zip_safe=False
+    zip_safe=False,
+    python_requires='>=3'
 )
